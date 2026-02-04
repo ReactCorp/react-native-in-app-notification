9dcdb07522a90bdd471bd84f38a347bb6833e31e
Maty Oirik <42285705+oirik@users.noreply.github.com>
2026-02-04 14:00:19 +0900

    (refs ReactCorp/Cocalero_mvp#7583)Fix notification position for Android edge-to-edge display (API 35+)
    Android API 35+ enables edge-to-edge by default, causing notifications
    to appear behind the status bar. This fix applies StatusBar.currentHeight
    as padding on Android 35+ devices.

--- src/Notification.js
+++ src/Notification.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Animated, StyleSheet } from 'react-native';
+import { Animated, Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { getStatusBarHeight, isIphoneX } from 'react-native-iphone-x-helper';
 import { ImagePropTypes } from 'deprecated-react-native-prop-types';
 
@@ -17,7 +17,7 @@ class Notification extends Component {
   constructor() {
     super();
 
-    this.heightOffset = isIphoneX() ? getStatusBarHeight() : 0;
+    this.heightOffset = isIphoneX() ? getStatusBarHeight() : (Platform.OS === 'android' && Platform.Version >= 35) ? StatusBar.currentHeight : 0;
 
     this.show = this.show.bind(this);
     this.showNotification = this.showNotification.bind(this);
@@ -147,21 +147,23 @@ class Notification extends Component {
           },
         ]}
       >
-        <NotificationBody
-          title={title}
-          message={message}
-          onPress={onPress}
-          isOpen={isOpen}
-          iconApp={iconApp}
-          icon={icon}
-          vibrate={vibrate}
-          onClose={() => {
-            //clear timeout
-            clearTimeout(this.currentNotificationInterval);
-            this.setState({ isOpen: false }, this.closeNotification);
-          }}
-          additionalProps={this.state.additionalProps}
-        />
+        <View style={{ paddingTop: this.heightOffset }}>
+          <NotificationBody
+            title={title}
+            message={message}
+            onPress={onPress}
+            isOpen={isOpen}
+            iconApp={iconApp}
+            icon={icon}
+            vibrate={vibrate}
+            onClose={() => {
+              //clear timeout
+              clearTimeout(this.currentNotificationInterval);
+              this.setState({ isOpen: false }, this.closeNotification);
+            }}
+            additionalProps={this.state.additionalProps}
+          />
+        </View>
       </Animated.View>
     );
   }
